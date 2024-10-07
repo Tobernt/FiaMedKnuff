bfb9a5b89c16ce09e356f1851da192c13428e0da
Tobernt <tobias_berntsson@hotmail.com>
2024-10-07 12:29:28 +0200

    Main Menu and Transition

--- App.xaml.cs
+++ App.xaml.cs
@@ -41,19 +41,11 @@ namespace FiaMedKnuff
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
+                // Create a new Frame and set it as the Window's content
                 rootFrame = new Frame();
-
                 rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
 
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
@@ -63,12 +55,9 @@ namespace FiaMedKnuff
             {
                 if (rootFrame.Content == null)
                 {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    // Navigate to the MainMenu page (instead of MainPage)
+                    rootFrame.Navigate(typeof(MainMenu), e.Arguments);
                 }
-                // Ensure the current window is active
                 Window.Current.Activate();
             }
         }
